189b42b5561c8de29fec8665c44a81c3bd96636c
MainShayne233 <shaynetremblay@hotmail.com>
2017-01-01 14:09:37 -0500

    updates user_socket

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -1,11 +1,12 @@
 defmodule CodeTogether.UserSocket do
   use Phoenix.Socket
-  
+
   channel "code_room:*", CodeTogether.CodeRoomChannel
 
   transport :websocket, Phoenix.Transports.WebSocket
 
   def connect(_params, socket) do
+    IO.puts "CONNECT"
     {:ok, socket}
   end
 
